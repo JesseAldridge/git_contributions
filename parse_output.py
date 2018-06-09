baf0b0941c1307fca1cedd3f92ee48f47c40c4f8
Jesse Aldridge <jesse.aldridge@airbnb.com>
2018-06-09 02:11:12 -0700

    add note

--- parse_output.py
+++ parse_output.py
@@ -16,5 +16,6 @@ mean = sum(email_to_loc.values()) / float(len(email_to_loc))
 print 'mean:', round(mean)
 
 print 'lines of code added + deleted across all repos since {}'.format(config['since'])
+print '(commits from the top quintile in each repo are excluded)'
 for email, loc in sorted(email_to_loc.iteritems(), key=lambda t: -t[1]):
   print '{:<40} {:<5} {:.2}'.format(email, loc, loc / mean)
